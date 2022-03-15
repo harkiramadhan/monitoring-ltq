62f58fab295ac9052523382ac3edbb18d0724df5
Ahmad Harki Ramadhan <harkiramadhan@icloud.com>
2022-03-15 10:56:58 +0700

    updates

--- assets/js/custom/ajax_tahunajar.js
+++ assets/js/custom/ajax_tahunajar.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
+    var activeCardDetail = localStorage.getItem('ta-card-detail')
+    if(activeCardDetail){
+        getDetail(activeCardDetail)
+    }
+
     $('.btn-detail').click(function(){
         var id = $(this).attr('data-id')
-        $.ajax({
-            url: siteUrl + 'tahunajar/detail',
-            type: 'get',
-            data: {id : id},
-            success: function(res){
-                $('.div-detail').html(res)
-            }
-        })
+        getDetail(id)
     })
 
     $('.btn-active').click(function(){
@@ -18,8 +16,21 @@ $(document).ready(function(){
             type: 'post',
             data: {id : id},
             success: function(res){
+                localStorage.setItem('ta-card-detail', id)
                 location.reload()
             }
         })
     })
+
+    function getDetail(id){
+        $.ajax({
+            url: siteUrl + 'tahunajar/detail',
+            type: 'get',
+            data: {id : id},
+            success: function(res){
+                $('.div-detail').html(res)
+                localStorage.setItem('ta-card-detail', id)
+            }
+        })
+    }
 })
